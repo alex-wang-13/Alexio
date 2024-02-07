fc204f535fdd93c70e1446df6ba12e155995fb2a
Alex Wang <iamalex11303@gmail.com>
2024-02-06 22:50:31 -0500

    renamed header component to profilepic

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
-import Header from '../components/Header';
+import Header from '../components/ProfilePic';
 import NavBar from '../components/NavBar';
 import Body from '../components/Body';
 
 function App() {
   return (
     <div>
-      <Header />
+      <ProfilePic />
       <NavBar />
       <Body />
     </div>
