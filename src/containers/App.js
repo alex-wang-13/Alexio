ec1cdb5908d67e42d036fd7b8baa03e60ad8ff2b
Alex Wang <iamalex11303@gmail.com>
2024-02-28 12:04:45 -0500

    implemented router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import HomeBody from '../components/HomeBody';
 
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-      <HomeBody />
-    </div>
+      <Routes>
+        <Route path="/" element={<HomeBody />} />
+        <Route />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
