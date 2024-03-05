34943d1d3c1b2810ac86e2c99b74207762628718
Alex Wang <iamalex11303@gmail.com>
2024-03-05 16:44:48 -0500

    used react dom link component instead of <a>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-black sticky-top">
       <div className="navbar-nav justify-content-around w-100">
-        <a href="/" className="nav-item nav-link align-self-center"><h4>Home</h4></a>
-        <a href="/about" className="nav-item nav-link align-self-center"><h4>About</h4></a>
-        <a href="/projects" className="nav-item nav-link align-self-center"><h4>Projects</h4></a>
+        <Link to="/" className="nav-item nav-link align-self-center"><h4>Home</h4></Link>
+        <Link to="/about" className="nav-item nav-link align-self-center"><h4>About</h4></Link>
+        <Link to="/projects" className="nav-item nav-link align-self-center"><h4>Projects</h4></Link>
       </div>
     </nav>
   );
 }
-
 
 export default NavBar;
