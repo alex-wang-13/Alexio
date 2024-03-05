99f0504724dab67273971590c84622607f1fb11a
Alex Wang <iamalex11303@gmail.com>
2024-03-05 18:58:36 -0500

    changed links to redirect in new tab

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,13 +16,13 @@ function HomePage() {
         <div className="col-6 p-0 d-flex flex-column">
           <div className="card d-flex align-items-stretch bg-secondary justify-content-center m-5 h-50">
             <div>
-              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' href="https://letterboxd.com/alexthedawg/"><p>letterboxd</p><img src={lblogo} alt='letterboxd logo' /></a></p>
+              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' target='blank_' href="https://letterboxd.com/alexthedawg/"><p>letterboxd</p><img src={lblogo} alt='letterboxd logo' /></a></p>
             </div>
             <div>
-              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' href="https://www.goodreads.com/user/show/57369610-alex"><p>goodreads</p><img src={grlogo} alt='goodreads logo' /></a></p>
+              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' target='blank_' href="https://www.goodreads.com/user/show/57369610-alex"><p>goodreads</p><img src={grlogo} alt='goodreads logo' /></a></p>
             </div>
             <div>
-              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' href="https://github.com/alex-wang-13"><p>github</p><img src={ghlogo} alt='github logo' /></a></p>
+              <p className='text-center p-4 m-0'><a className='link-dark text-uppercase text-decoration-none' target='blank_' href="https://github.com/alex-wang-13"><p>github</p><img src={ghlogo} alt='github logo' /></a></p>
             </div>
           </div>
         </div>
