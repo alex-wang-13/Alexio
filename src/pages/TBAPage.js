509d1ea030fb42bc23d29a8baaadbba8b197921d
Alex Wang <iamalex11303@gmail.com>
2024-03-05 18:58:10 -0500

    created stand in page for tba

---
+++ src/pages/TBAPage.js
@@ -0,0 +1,11 @@
+export default function TBAPage() {
+    return (
+        <>
+            <div className="container-fluid bg-dark min-vh-100 p-4">
+                <div className="d-flex flex-column">
+                    <p className="text-center bg-secondary">Coming soon...</p>
+                </div>
+            </div>
+        </>
+    );
+}
