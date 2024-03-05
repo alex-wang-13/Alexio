1ab22cf257629c2b8df87ab05c841457f89c1407
Alex Wang <iamalex11303@gmail.com>
2024-03-05 18:14:24 -0500

    removed sticky from navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-black sticky-top">
+    <nav className="navbar navbar-expand-md navbar-dark bg-black m-0">
       <div className="navbar-nav justify-content-around w-100">
         <Link to="/" className="nav-item nav-link align-self-center"><h4>Home</h4></Link>
         <Link to="/about" className="nav-item nav-link align-self-center"><h4>About</h4></Link>
