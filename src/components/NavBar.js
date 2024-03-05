e4706694d862dd241121089007eeb3a5f4beff8a
Alex Wang <iamalex11303@gmail.com>
2024-03-05 18:17:14 -0500

    replaced about with blog tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     <nav className="navbar navbar-expand-md navbar-dark bg-black m-0">
       <div className="navbar-nav justify-content-around w-100">
         <Link to="/" className="nav-item nav-link align-self-center"><h4>Home</h4></Link>
-        <Link to="/about" className="nav-item nav-link align-self-center"><h4>About</h4></Link>
+        <Link to="/blog" className="nav-item nav-link align-self-center"><h4>Blog</h4></Link>
         <Link to="/projects" className="nav-item nav-link align-self-center"><h4>Projects</h4></Link>
       </div>
     </nav>
