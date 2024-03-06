201ccfee50022e32420f4b684ee2132228968a1f
Alex Wang <iamalex11303@gmail.com>
2024-03-05 19:54:58 -0500

    Added blog page template

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,7 +1,13 @@
+import image from "../images/pfp.jpg";
+import Post from "../components/Post";
+
 export default function BlogPage() {
+
     return (
-        <>
-            blog
-        </>
+        <div className="container-fluid bg-dark min-vh-100 ">
+            <Post title="Title" href={image} blurb="Some text..." />
+            <Post title="Title" href={image} blurb="Some text..." />
+            <Post title="Title" href={image} blurb="Some text..." />
+        </div>
     );
 }
