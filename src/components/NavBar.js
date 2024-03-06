d5529c0a2877ba802690b0b459da5584c3110560
Alex Wang <iamalex11303@gmail.com>
2024-03-05 19:53:24 -0500

    re-added sticky top

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-black m-0">
+    <nav className="navbar navbar-expand-md navbar-dark bg-black m-0 sticky-top">
       <div className="navbar-nav justify-content-around w-100">
         <Link to="/" className="nav-item nav-link align-self-center"><h4>Home</h4></Link>
         <Link to="/blog" className="nav-item nav-link align-self-center"><h4>Blog</h4></Link>
